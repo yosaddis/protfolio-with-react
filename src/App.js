842b3c1af8377f337894b4517832e7cf39fa0123
yosaddis <email.to.yoseph@gmail.com>
2023-10-12 16:21:27 +0300

    Update app js to render components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
+    <main className="l-main">
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="../assets/css/styles.css" />
+        <link href='https://cdn.jsdelivr.net/npm/boxicons@2.0.5/css/boxicons.min.css' rel='stylesheet' />
+        <title>Yoseph's Portfolio website</title>
+        <script src="https://unpkg.com/scrollreveal"></script>
+        <script src="assets/js/main.js"></script>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Header />
+     <Home />
+     <About />
+     <Skills />
+     <Works />
+     <Contact />
+     <Footer />
     </div>
+  </main>
   );
 }
 
