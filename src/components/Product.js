43c1c130faaca6f02b2c3d890c0b0930c5936637
yosaddis <email.to.yoseph@gmail.com>
2023-10-29 22:06:56 +0300

    Add Product pafe

---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = () => {
+    return (
+        <section className="contact section" id="contact">
+           
+        </section>
+    );
+};
+
+export default Product;
