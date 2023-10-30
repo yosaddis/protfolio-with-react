baf5f5e3ba340efea75f3c4e6ed6b8a39f7611c0
yosaddis <email.to.yoseph@gmail.com>
2023-10-30 13:14:39 +0300

    Add testimonials card component

---
+++ src/components/TestemonialCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/styles.css'
+import perfil from '../assets/img/perfil.png'
+
+const TestimonialCard = () => {
+    return (
+        <div>
+          Testimonila Card
+        </div>
+    );
+};
+
+export default TestimonialCard;
