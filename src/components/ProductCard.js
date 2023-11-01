e5ea9a9a003d109b152e7c57e32f2a819ffa3bda
yosaddis <email.to.yoseph@gmail.com>
2023-11-01 17:07:04 +0300

    update the product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import '../assets/css/ProductCard.css';
+import "../assets/css/styles.css";
 
-const ProductCard = () => {
+const ProductCard = ({ image, project_title, description, link }) => {
     return (
-        <section className="contact section" id="contact">
-           
-        </section>
+        <div className="product-card">
+        <div className="product__img">
+            <img src={image} alt="skill icon"/>
+        </div>
+        <h3 className="skill-name">{project_title}</h3>
+        <ul className="languages-list">
+           <p>{description}</p>
+        </ul>
+        <a href={link} target="_blank" rel="noopener noreferrer" className="project-link-button">Read More</a>
+    </div>
     );
 };
 
