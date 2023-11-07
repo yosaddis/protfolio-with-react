2cf26a0e83f174b9a2e31f701f1a29e869eeb394
yosaddis <email.to.yoseph@gmail.com>
2023-11-07 18:45:27 +0300

    update the header componenet for mobile menue toggling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,55 +1,29 @@
-import { React, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../assets/css/styles.css';
 
 const Header = () => {
-  // Menu toggle function
-  const showMenu = (toggleId, navId) => {
-    const toggle = document.getElementById(toggleId);
-    const nav = document.getElementById(navId);
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
-    if (toggle && nav) {
-      toggle.addEventListener('click', () => {
-        nav.classList.toggle('show');
-      });
-    }
-  };
-
-  // Scroll to section function
-  const linkAction = (navMenu) => {
-    // When we click on each nav__link, we remove the show-menu class
-    navMenu.classList.remove('show');
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+    console.log('clicked');
+    console.log(isMenuOpen);
   };
 
   useEffect(() => {
-    // Initialize menu toggle and scroll functionality when the component mounts
-    showMenu('nav-toggle', 'nav-menu');
     document.title = "Yoseph's Portfolio Website";
-    const navMenu = document.getElementById('nav-menu');
-    const navLink = document.querySelectorAll('.nav__link');
-
-    navLink.forEach((link) => link.addEventListener('click', () => linkAction(navMenu)));
-
-    // Scroll to sections on link click
-    // sections.forEach((section) => {
-    //   const sectionId = section.id;
-    //   const navLink = document.querySelector(`.nav__link[href="#${sectionId}"]`);
-    //   navLink.addEventListener('click', () => {
-    //     document.querySelector('.nav__link.active').classList.remove('active');
-    //     navLink.classList.add('active');
-    //   });
-    // });
   }, []);
 
   return (
     <header className="l-header">
       <nav className="nav bd-grid">
         <div>
-          <a href="https://www.google.com" className="nav__logo">Yoseph</a>
+          <a href="https://yoseph-portfolio.onrender.com/" className="nav__logo">Yoseph</a>
         </div>
 
-        <div className="nav__menu" id="nav-menu">
+        <div className={`nav__menu ${isMenuOpen ? 'show' : ''}`} id="nav-menu">
           <ul className="nav__list">
-            <li className="nav__item"><a href="#home" className="nav__link active">Home</a></li>
+            <li className="nav__item"><a href="#home" className="nav__link">Home</a></li>
             <li className="nav__item"><a href="#about" className="nav__link">About</a></li>
             <li className="nav__item"><a href="#skills" className="nav__link">Skills</a></li>
             <li className="nav__item"><a href="#work" className="nav__link">Projects</a></li>
@@ -58,31 +32,10 @@ const Header = () => {
           </ul>
         </div>
 
-        <div className="nav__toggle" id="nav-toggle">
+        <button type="button" className="nav__toggle" id="nav-toggle" onClick={toggleMenu}>
+          {' '}
           <i className="bx bx-menu" />
-        </div>
-        <div className="offcanvas-body">
-          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li className="nav-item">
-              HomeM
-            </li>
-            <li className="nav-item">
-              About
-            </li>
-            <li className="nav-item">
-              Skills
-            </li>
-            <li className="nav-item">
-              Project
-            </li>
-            <li className="nav-item">
-              Product
-            </li>
-            <li className="nav-item">
-              ContactM
-            </li>
-          </ul>
-        </div>
+        </button>
       </nav>
     </header>
   );
